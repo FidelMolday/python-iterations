50dba789381b52c91c34362041582e2e8a5cbc19
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-02-29 09:49:58 +0300

    Create iterations.py

---
+++ iterations.py
@@ -0,0 +1,9 @@
+def get_squared_numbers(numbers):
+    squared_numbers=[]
+    for n in numbers:
+        squared_numbers.append(n*n)
+    return squared_numbers   
+
+    numbers = [2,5,8,9]
+    get_squared_numbers(numbers)
+    #returns(4,25,64,81)
